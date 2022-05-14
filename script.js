19e56d9bc8cae82e8e8ee4191bf04b807d4bd120
Luke Eades <lukegeades@gmail.com>
2022-05-13 21:07:20 -0700

    fix RPS

--- script.js
+++ script.js
@@ -3,9 +3,8 @@ function computerPlay(){
         return "rock";
     }else if(getRandomInt(1,3) == 2){
         return "paper";
-    }else{
-        return "scissors";
-    }       
+    }
+    return "scissors";
 }
 function getRandomInt(min, max) {
     min = Math.ceil(min);
@@ -13,16 +12,17 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min) + 
     min);
 }
-console.log(computerPlay());
-let userPick = prompt("Choose either rock, paper, or scissors");
+
+
 function choose(){
-    if(computerPlay() == userPick.toLowerCase()){
-        return "It's a tie";
-    }else if(computerPlay() === "rock" && userPick.toLowerCase() === "scissors" || computerPlay() === "paper" && userPick.toLowerCase() === "rock" || computerPlay() === "scissors" && userPick.toLowerCase() === "paper"){
+    let userPick = prompt("Choose either rock, paper, or scissors");
+    if((computerPlay() === "rock" && userPick.toLowerCase() === "scissors") || (computerPlay() === "paper" && userPick.toLowerCase() === "rock") || (computerPlay() === "scissors" && userPick.toLowerCase() === "paper")){
+        return "Computer wins";
+    }else if((userPick.toLowerCase() === "rock" && computerPlay() === "scissors") || (userPick.toLowerCase() === "paper" && computerPlay() === "rock") || (userPick.toLowerCase() === "scissors" && computerPlay() === "paper")){
         return "User wins";
-    }else if(userPick.toLowerCase() === "rock" && computerPlay() === "scissors" || userPick.toLowerCase() === "scissors" && computerPlay() === "paper" || userPick.toLowerCase() === "paper" && computerPlay() === "rock"){
-        return "User wins";
+    }else{
+        return "it's a tie";
     }
 
 }
-console.log(choose());
+alert(choose());
